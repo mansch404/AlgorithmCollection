9a2bcce35a2787860e7db39d3d9e603f0a01928a
mansch404 <mansch404@gmail.com>
2021-11-26 12:58:50 +0100

    Added Chess Python File

---
+++ chess/chess.py
@@ -0,0 +1,13 @@
+
+class Chess():
+
+    color = "w"
+
+    def __init__(self):
+        pass
+
+    def getChess():
+        for x in range(8):
+            for y in range(8):
+                if x % 2 == 0:
+                    color = "b"
