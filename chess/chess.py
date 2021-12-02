1f86b914ffb61fe298d66d87acd30933196fa45b
mansch404 <manuel-asbeck5@t-online.de>
2021-12-02 21:29:01 +0100

    Added to ChessAlgorithm

--- chess/chess.py
+++ chess/chess.py
@@ -1,13 +1,20 @@
 
-class Chess():
-
-    color = ""
-
-    def __init__(self, stanColor):
-        color = stanColor
-
-    def getChess():
+def runAlgorithm():
         for x in range(8):
             for y in range(8):
+                if y == 7:
+                    print()
                 if x % 2 == 0:
-                    color = "w"
+                    if y % 2 == 0:
+                       color = "w"
+                    else: 
+                        color = "b"
+                else:
+                    if y % 2 == 0:
+                       color = "b"
+                    else: 
+                        color = "w"
+                
+                print(color, end='')
+
+runAlgorithm()
