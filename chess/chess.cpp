fc5218967c284b505f801455c395530cca91b119
mansch404 <manuel-asbeck5@t-online.de>
2021-12-05 21:44:36 +0100

    Added Comments

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// main method
 int main() {
     for (int x = 0; x < 8;x++) {
         for (int y = 0; y < 8;y++) {
             if(x % 2 == 0) {
-                cout << "w";
+                cout << "w"; // prints out the white color
             } else {
-                cout << "b";
+                cout << "b"; // prints out the black color
             }
 
             if(x == 7) {
